8cc1573511e9fa2b172f9c050e3103552830ae36
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-31 11:55:27 -0600

    next update

--- src/components/card.js
+++ src/components/card.js
@@ -11,9 +11,12 @@ class Card extends Component {
             color: 'BLUE',
             pluralNoun: ''
         }
+
+        this.handleInputChange= this.handleInputChange.bind(this);
     }
 
     handleInputChange() {
+        console.log('this is the value')
         this.setState({color: 'red' })
     }
 
@@ -22,12 +25,19 @@ class Card extends Component {
         return (
             <div className="card">
             <h1>{this.state.color}</h1>
-            <input />
+            <input onChange={this.handleInputChange}/>
             </div>
         )
 
         return (
             <div className="card">
+                { Input('Color') }
+                { Input('Plural Noun') }
+            </div>
+        )
+    }
+}
+ame="card">
                 { Input('Color') }
                 { Input('Plural Noun') }
             </div>
