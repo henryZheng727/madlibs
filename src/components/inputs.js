9857e7f2aafc9348031ce974946f66caba90d6fb
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-31 10:42:06 -0600

    another update

---
+++ src/components/inputs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+ const Input = (title) => {
+    return (
+        <div className="input">
+            <input />
+            <label>{title}</label>
+        </div>
+    )
+}
+ export default Input; 
